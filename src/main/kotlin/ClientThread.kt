676eb6391ee5a9d98cbfd44fb7506193b414c661
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2021-01-09 17:43:01 +0300

    Server done

--- src/main/kotlin/ClientThread.kt
+++ src/main/kotlin/ClientThread.kt
@@ -1,2 +1,22 @@
-class ClientThread {
+import java.lang.Exception
+import java.net.Socket
+
+class ClientThread(private val dataBase: DataBase, private val message: String, private val port: Int): Thread() {
+
+    init { this.start() } // стартую тред при инициализации
+    override fun run() {
+        println(this.hashCode())
+        val answer = dataBase.syncFun(message) // запускаю логику вычислений - синхорнный метод
+        val socket = Socket("127.0.0.1", port) // сокет для отправки результата вычислений клиенту
+        if (socket.isConnected) { // проверяем есть ли коннект
+            socket.getOutputStream().write(answer.toByteArray()) // создаем сообщение и отпрвляем в стрим
+            try {
+                socket.close() // закрываем сокет
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            println("${socket.isClosed} Socket.close")
+            if (socket.isClosed) this.interrupt() // закрываем поток если сокет закрылся
+        }
+    }
 }
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,2 +1,26 @@
+import java.io.BufferedReader
+import java.lang.Exception
+import java.lang.Thread.sleep
+import java.net.ServerSocket
+
 class Server {
+    fun startServer() {
+        try {
+            val dataBase = DataBase() // создаю объект класса для работы с БД (разделяемый ресурс)
+            dataBase.download() // делаю инициализацию продуктов из БД
+            ServerSocket(3345).use { server -> // открываю прослушку сокета на 3345 порту
+                println("Server is running on port ${server.localPort}")
+                while (true) {
+                    val client = server.accept() // подключаю клиентов
+                    if (client.isConnected) { // если клиент законнектился, то стартую свой тред
+                        println("Client connected: ${client.inetAddress.hostAddress}:${client.localPort}")
+                        val messageParts = BufferedReader(client.getInputStream().reader()).readLine().split("|")
+                        println("Server: get message [$messageParts]")
+                        sleep(10)
+                        ClientThread(dataBase, messageParts[0], messageParts[1].toInt())
+                    }
+                }
+            }
+        } catch (e: Exception) { e.printStackTrace() } // если сокет не создался или что-то пошло не так принчу стактрейс
+    }
 }
