e2f6cae0e333ab7514cb110eaa20f699f49ce207
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2021-01-09 17:41:23 +0300

    Server done

--- src/main/kotlin/DataBase.kt
+++ src/main/kotlin/DataBase.kt
@@ -8,7 +8,7 @@ class DataBase {
     var fourthProduct: Int? = null
     var fifthProduct: Int? = null
 
-    fun download() {
+    fun download() { // выгружаем данные из БД
         val file = File("dataBase").bufferedReader().readLines()
         val listOfCurrentValue = parser(file)
         firstProduct = listOfCurrentValue[0]
@@ -19,7 +19,7 @@ class DataBase {
     }
 
     @Synchronized
-    private fun upload(): String {
+    private fun upload(): String { // метод который обновляет данные в БД
         val file = File("dataBase").bufferedReader().readLines()
         val listOfNewData = mutableListOf<String>()
         val listOfCurrentValue =  mutableListOf<Int?>()
@@ -42,22 +42,27 @@ class DataBase {
     }
 
      @Synchronized
-     private fun subtraction(str: String) {
+     private fun subtraction(str: String) { // проводим операцию вычитания с разделяемым ресурсом (БД)
          val part = str.split(" ")
          firstProduct = firstProduct?.minus(part[0].toInt())
+         if (firstProduct!! < 0) firstProduct = 0
          secondProduct = secondProduct?.minus(part[1].toInt())
+         if (secondProduct!! < 0) secondProduct = 0
          thirdProduct = thirdProduct?.minus(part[2].toInt())
+         if (thirdProduct!! < 0) thirdProduct = 0
          fourthProduct = fourthProduct?.minus(part[3].toInt())
+         if (fourthProduct!! < 0) fourthProduct = 0
          fifthProduct = fifthProduct?.minus(part[4].toInt())
+         if (fifthProduct!! < 0) fifthProduct = 0
     }
 
-    @Synchronized
+    @Synchronized // метод для выполнения нескольких операций над БД синхроннизированно
     fun syncFun(str: String): String {
         subtraction(str)
         return upload()
     }
 
-    private fun parser(list: List<String>): MutableList<Int> {
+    private fun parser(list: List<String>): MutableList<Int> { // парсер данных из бд
         val resultList = mutableListOf<Int>()
         for (i in list) {
             val part = i.split("_")
